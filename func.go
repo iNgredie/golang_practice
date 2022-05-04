4590613cb6b42d92c771937ee5b27743ff5cdac4
Aleksandr Durnev <sandslash@bk.ru>
2022-05-05 01:09:44 +0400

    func practice

---
+++ func.go
@@ -0,0 +1,26 @@
+package practice
+
+import "fmt"
+
+func addPrefix(origin string) string {
+	return "prefix_" + origin
+}
+
+func addPrefixWithErr(origin string) (string, error) {
+	return "prefix " + origin, nil
+}
+
+func addPrefixWithLen(origin string) (res string, length int) {
+	res = "prefix_" + origin
+	length = len(res)
+	return
+}
+
+func main() {
+	myString := addPrefix("my_string")
+	fmt.Println(myString) // prefix_my_string
+
+	myString, err := addPrefixWithErr("error_string")
+	fmt.Println(err) // nil
+	fmt.Println(myString) // prefix_error_string
+}
