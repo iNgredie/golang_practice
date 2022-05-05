e1a24c623c3831784c2ae203e16a69a54e7be194
Aleksandr Durnev <sandslash@bk.ru>
2022-05-05 22:10:07 +0400

    struct_2_add

--- struct_2.go
+++ struct_2.go
@@ -11,7 +11,7 @@ type Client struct {
 	ID   int64
 	Name string
 	Age  int
-	IMG  Avatar
+	IMG  *Avatar
 }
 
 func main() {
@@ -19,8 +19,12 @@ func main() {
 	_ = i
 
 	client := Client{}
+	client.IMG = new(Avatar)
+	client.IMG = &Avatar{}
 	fmt.Printf("%#v\n", client)
 	// main.Client{ID:0, Name:"", Age:0, IMG:main.Avatar{URL:"", Size:0}}
+	// main.Client{ID:0, Name:"", Age:0, IMG:(*main.Avatar)(nil)}
+	// main.Client{ID:0, Name:"", Age:0, IMG:(*main.Avatar)(0xc000096440)}
 
 	// client.IMG = new(Avatar)
 	updateAvatar(&client)
